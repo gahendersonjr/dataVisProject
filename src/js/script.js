fffed231b2bc195fc1fa516d83cd2f9318724d3d
Alan Henderson <ahenderson@mastercontrol.com>
2018-11-13 13:20:08 -0700

    adding data file, code to parse the data, and other boilerplate stuff

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+let countries = {};
+let years = {};
+
+d3.csv("data/lifeExpectancy.csv", function (d) {
+  d.DATA_QUALITY = d.DATA_QUALITY.slice(3, d.DATA_QUALITY.length);
+  return d;
+}).then(function(data) {
+  for (let i in data){
+    let country = data[i].AREA;
+    let year = data[i].YEAR;
+
+    if (country in countries) {
+      countries[country].push(data[i]);
+    } else {
+      countries[country]=[data[i]];
+    }
+
+    if (year in years) {
+      years[year].push(data[i]);
+    } else {
+      years[year]=[data[i]];
+    }
+  }
+});
+
+console.log(years);
+console.log(countries);
