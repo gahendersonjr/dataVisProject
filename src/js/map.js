7a38c37b7516a2ec82ae0e754b0d52aadf79b01b
Alan Henderson <gahendersonjr@gmail.com>
2018-12-02 18:37:59 -0700

    basic map zoom

--- src/js/map.js
+++ src/js/map.js
@@ -1,8 +1,21 @@
 
 class Map {
   constructor(countries) {
+    this.svg = d3.select("#map1")
+    this.g = this.svg.append("g");
     this.projection = d3.geoMercator().scale(200).translate([590, 400]);
+
+    this.path = d3.geoPath()
+      .projection(this.projection);
     this.countries = countries;
+
+    this.zoom = d3.zoom()
+      .scaleExtent([1, 8])
+      .on("zoom", function(d){
+        this.g.attr("transform", d3.event.transform);
+      }.bind(this));
+
+    this.svg.call(this.zoom);
 
     this.colorScale = d3.scaleLinear()
       .domain([0, 100])
@@ -11,22 +24,18 @@ class Map {
     this.rotateScale = d3.scaleLinear()
       .domain([-1, 1])
       .range([180, 0]);
-
-    this.svg = d3.select("#map1");
     }
 
   drawMap(world) {
     world = topojson.feature(world,world.objects.countries);
-    let path = d3.geoPath()
-      .projection(this.projection);
-    let enter = this.svg.selectAll("path")
+    let enter = this.g.selectAll("path")
       .data(world.features)
       .enter();
 
     enter.append("path")
       .attr("class", "countries")
       .attr("id", d => d.id)
-      .attr("d", path)
+      .attr("d", this.path)
       .attr("fill", "lightGray")
       .attr("stroke", "white");
 
@@ -34,7 +43,7 @@ class Map {
         .classed("arrow", true)
         .classed("hasData", true)
         .attr("id", d => d.id + "Arrow")
-        .attr("d", path)
+        .attr("d", this.path)
         .attr("fill", "none")
         .classed("inactive", true)
    	    .on("click", d => time.toggleCountry(d.id) )
@@ -45,10 +54,11 @@ class Map {
   this.updateCountry(year);
   this.createPatterns();
   this.updateArrows(year);
+
   }
 
   createPatterns(){
-    this.svg.append("defs")
+    this.g.append("defs")
       .selectAll("pattern")
       .data(this.countries)
       .enter()
@@ -98,18 +108,18 @@ class Map {
     for(let i in this.countries) {
       let country = countries[i];
        if(country[year-interval] && country[year]){
-         this.svg.select("#" + country.geo + "Arrow")
+         this.g.select("#" + country.geo + "Arrow")
           .classed("inactive", !checked);
 
          let percent_change = (country[year] - country[year-interval])/country[year];
          rotate = this.rotateScale(percent_change);
 
-         let pattern = this.svg.select("#"  + country.geo + "Pattern");
+         let pattern = this.g.select("#"  + country.geo + "Pattern");
 
          pattern
           .attr("patternTransform", "rotate(" + rotate +")")
 
-        this.svg.select("#" + country.geo + "Arrow")
+        this.g.select("#" + country.geo + "Arrow")
           .attr("fill", "url(#"  + country.geo + "Pattern)");
 
         pattern.selectAll("*")
@@ -123,7 +133,7 @@ class Map {
             }
           });
        } else {
-         this.svg.select("#" + country.geo + "Arrow")
+         this.g.select("#" + country.geo + "Arrow")
           .classed("inactive", true);
        }
      }
